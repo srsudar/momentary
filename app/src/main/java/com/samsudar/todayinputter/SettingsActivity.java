7f33d9945709740e9919797880f387b8f7d9cc53
Sam Sudar <sudar.sam@gmail.com>
2016-05-25 14:47:36 -0700

    Add ActionBar to preference activity

--- app/src/main/java/com/samsudar/todayinputter/SettingsActivity.java
+++ app/src/main/java/com/samsudar/todayinputter/SettingsActivity.java
@@ -6,13 +6,12 @@ import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.preference.Preference;
-import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
 import android.view.MenuItem;
 
 
-public class SettingsActivity extends PreferenceActivity {
+public class SettingsActivity extends AppCompatPreferenceActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
